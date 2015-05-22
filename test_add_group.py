6784692c14596cb150c3380551335f4033e290be
Igor Golubenkov <i.golubenkov@mail.ru>
2015-05-22 17:24:56 +0300

    Создан проект PyCharm ( исправления 1.2)

--- test_add_group.py
+++ test_add_group.py
@@ -4,7 +4,6 @@ import unittest
 
 def is_alert_present(wd):
     try:
-        wd.switch_to_alert().text
         return True
     except:
         return False
@@ -13,10 +12,11 @@ class test_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-    
-    def test_add_group(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -24,9 +24,13 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
-        wd.find_element_by_link_text("add new").click()
+
+    def open_group_page(self, wd):
         wd.find_element_by_link_text("groups").click()
+    def creat_group(self, wd):
+        # init group creation
         wd.find_element_by_name("new").click()
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("qwe")
@@ -36,10 +40,26 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("qwe")
+        # submit group creation
         wd.find_element_by_name("submit").click()
+
+    def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
-    
+
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_group_page(wd)
+        self.creat_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+
     def tearDown(self):
         self.wd.quit()
 
