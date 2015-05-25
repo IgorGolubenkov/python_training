67c8013c70d9a2c3004ec8200d34ca0820fc2542
Igor Golubenkov <i.golubenkov@mail.ru>
2015-05-25 18:22:28 +0300

    Test_add_Contact

---
+++ contact.py
@@ -0,0 +1,25 @@
+__author__ = 'user'
+
+
+class Contact:
+
+    def __init__(self, name_contact, mname_contact, lname_contact, header, enterprise, nick, location, fax,
+                      mail_1, mail_2, web, h_phone, m_phone, j_phone, location_2, note, phone_2):
+        self.name_contact="igor"
+        self.mname_contact="igor2"
+        self.lname_contact="igor3"
+        self.nick="nick"
+        self.header="dir"
+        self.enterprise="app"
+        self.location="street"
+        self.j_phone="09876"
+        self.mail_1="igor@ya.ru"
+        self.mail_2="igor2@ya.ru"
+        self.web="www"
+        self.phone_2="home phone"
+        self.h_phone="54321"
+        selfm_phone="67890"
+        self.location_2="street-street"
+        self.note="non"
+        self.fax="dom"
+        self.m_phone="67890"
