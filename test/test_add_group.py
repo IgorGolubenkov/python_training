ecd3b4393c000c5859918129cfd224a889c1ef80
Igor Golubenkov <i.golubenkov@mail.ru>
2015-06-01 10:50:52 +0300

    Созданы пакеты для разных зон ответственности (add_group)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Application
+
+from model.group import Group
+from fixture.application import Application
+
 
 @pytest.fixture
 def app(request):
