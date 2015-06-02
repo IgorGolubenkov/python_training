3371e9ca8ad878a4fcd61b5284c1ee229f93712a
Igor Golubenkov <i.golubenkov@mail.ru>
2015-06-02 10:50:58 +0300

    Создан тест для удаления группы, создана фикстура для всех тестов. (version 1.0)

--- fixture/group.py
+++ fixture/group.py
@@ -6,13 +6,13 @@ class GroupHelper:
         def __init__(self, app):
             self.app = app
 
-        def open_group_page(self):
+        def open_groups_page(self):
             wd = self.app.wd
             wd.find_element_by_link_text("groups").click()
 
         def create(self, group):
             wd = self.app.wd
-            self.open_group_page()
+            self.open_groups_page()
             # init group creation
             wd.find_element_by_name("new").click()
             # fill group form
@@ -29,8 +29,20 @@ class GroupHelper:
             wd.find_element_by_name("submit").click()
             self.return_to_groups_page()
 
+        def delete_first_groups(self):
+            wd = self.app.wd
+            self.open_groups_page()
+            # select first group
+            wd.find_element_by_name("selected[]").click()
+            # submit deletion
+            wd.find_element_by_name("delete").click()
+            self.return_to_groups_page()
+
+
         def return_to_groups_page(self):
             wd = self.app.wd
             wd.find_element_by_link_text("group page").click()
+
+
 
 
