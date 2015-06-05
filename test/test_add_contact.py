b634dcea5f9f0473c290cca06b1cb6c116fa48e5
Igor Golubenkov <i.golubenkov@mail.ru>
2015-06-05 17:15:25 +0300

    Реализована интеллектуальная фикстура (проверка валидности фикстуры перед каждым тестом)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,21 +3,9 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.open_home_page()
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(name_contact="igor", mname_contact="igor2", lname_contact="igor3",
     nick="nick", header="dir", enterprise="app", location="street", j_phone="09876", mail_1="igor@ya.ru",
     mail_2="igor2@ya.ru", web="www", phone_2="home phone", h_phone="54321", m_phone="67890", location_2="street-street",
     note="non",fax="dom", byear="1991", ayear="1991"))
-    app.session.logout()
 
-
-def test_add_contact(app):
-    app.open_home_page()
-    app.session.login(username="admin", password="secret")
-    app.contact.create(Contact(name_contact="igor", mname_contact="igor2", lname_contact="igor3",
-    nick="nick", header="dir", enterprise="app", location="street", j_phone="09876", mail_1="igor@ya.ru",
-    mail_2="igor2@ya.ru", web="www", phone_2="home phone", h_phone="54321", m_phone="67890", location_2="street-street",
-    note="non",fax="dom", byear="1991", ayear="1991"))
-    app.session.logout()
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,11 +3,10 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="New group"))
-    app.session.logout()
 
-#def test_modify_group_header(app):
-    #app.session.login(username="admin", password="secret")
-    #app.group.modify_first_group(Group(header="New header"))
-    #app.session.logout()
+def test_modify_group_header(app):
+    app.group.modify_first_group(Group(header="New header"))
+
+def test_modify_group_footer(app):
+    app.group.modify_first_group(Group(footer="New footer"))
