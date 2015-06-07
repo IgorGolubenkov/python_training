66bf2aad2fbf05897f7199dc50a1fd64695fc190
Igor Golubenkov <i.golubenkov@mail.ru>
2015-06-07 10:33:10 +0300

    Проверка предусловий для теста редактирования групп(рабочая версия)

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,4 +4,6 @@ from model.group import Group
 
 
 def test_edit_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="игорь"))
     app.group.edit_first_group(Group(name="жанна", header="замена", footer="другое"))
