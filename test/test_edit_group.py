de562043fd4aa4cbd034f9666508046e32e9d47d
Igor Golubenkov <i.golubenkov@mail.ru>
2015-06-11 15:00:16 +0300

    Создание и сравнение списков групп.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,6 +4,10 @@ from model.group import Group
 
 
 def test_edit_first_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="игорь"))
     app.group.edit_first_group(Group(name="жанна", header="замена", footer="другое"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+
