26c10c1c1a30254a6f4a09dd0d4d0b482a4f00f7
Igor Golubenkov <i.golubenkov@mail.ru>
2015-06-11 17:26:03 +0300

    Сортировка списка перед сравнением для тестов групп (кроме редактирования группы).

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,13 +5,20 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="игорь", header="логин", footer="комментарий"))
+    group = Group(name="игорь", header="логин", footer="комментарий")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 
 def test_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,9 +4,9 @@ from model.group import Group
 
 
 def test_edit_first_group(app):
-    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="игорь"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="жанна", header="замена", footer="другое"))
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
