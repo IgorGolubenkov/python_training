82eb035b4d887c4ff3570f38878663ba87a9e1ae
Igor Golubenkov <i.golubenkov@mail.ru>
2015-06-16 20:08:04 +0300

    исправления

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,10 +4,11 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.create(Contact(firstname="igor", middlename="igor2", lastname="igor3",
+    contact = Contact(firstname="igor", middlename="igor2", lastname="igor3",
     nickname="nick", title="dir", company="app", address="street", home="09876", mobile="54321", work="777",
     fax="222", email="qwerty@yandex.ru", email2="igor@ya.ru", email3="igor2@ya.ru", homepage="www",
-    byear="1991", ayear="1991", phone2="home phone", address2="street-street", notes="non"))
+    byear="1991", ayear="1991", phone2="home phone", address2="street-street", notes="non")
+    app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
 
