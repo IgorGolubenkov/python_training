bcab6cbd48670c818b8e0b7f032c05a76e5637dd
Igor Golubenkov <i.golubenkov@mail.ru>
2015-07-09 17:40:48 +0300

    чтение данных во время теста создания нового контакта из файла в формате JSON

--- generator/contact.py
+++ generator/contact.py
@@ -2,7 +2,7 @@
 from model.contact import Contact
 import random
 import string
-import json
+import jsonpickle
 import os.path
 import getopt
 import sys
@@ -54,4 +54,5 @@ testdata = [Contact(firstname="", middlename="", lastname="",nickname="", title=
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
