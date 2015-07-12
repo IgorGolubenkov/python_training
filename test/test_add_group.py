525b34d22bb46b7c4b9f9510d389a2b7ad453330
Igor Golubenkov <i.golubenkov@mail.ru>
2015-07-12 17:29:25 +0300

    убрано хеширование из теста создания групп.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,6 +8,5 @@ def test_add_group(app, db, json_groups):
     old_groups = db.get_group_list()
     app.group.create(group)
     new_groups = db.get_group_list()
-    assert len(old_groups) + 1 == app.group.count()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
