071efafd439fd0d84da8ff72ef655adfe1fb3340
Igor Golubenkov <i.golubenkov@mail.ru>
2015-07-18 23:06:41 +0300

    В тест добавления контакта добавлены удаляемые проверки UI

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,7 +5,7 @@ import random
 from random import randrange
 
 
-def test_edit_group_by_id(app, db):
+def test_edit_group_by_id(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="игорь"))
     old_groups = db.get_group_list()
@@ -17,6 +17,9 @@ def test_edit_group_by_id(app, db):
     old_groups.remove(group)
     new_groups.remove(edit_data)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        ui_assert_db_list = db.get_group_list()
+        assert sorted(ui_assert_db_list, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 
 
