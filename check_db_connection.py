52c874d7466c0375dda3ac368b18f2bd3012b0a7
Igor Golubenkov <i.golubenkov@mail.ru>
2015-07-19 12:57:14 +0300

    создана ORM фикстура

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,19 +1,31 @@
 
-import mysql.connector
 from fixture.db import DbFixture
+from fixture.orm import ORM_fixture
 
 
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+db = ORM_fixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    contacts = db.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
-    groups = db.get_group_list()
-    for group in groups:
-        print(group)
-    print(len(groups))
+    l = db.get_contact_list()
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    db.destroy()
+    pass#db.destroy()
+
+
+#db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+#try:
+    #contacts = db.get_contact_list()
+    #for contact in contacts:
+        #print(contact)
+    #print(len(contacts))
+    #groups = db.get_group_list()
+    #for group in groups:
+        #print(group)
+    #print(len(groups))
+#finally:
+    #db.destroy()
 
